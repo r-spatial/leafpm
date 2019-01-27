2cd3dc69d83fbf3b71a151bac38c350bf87e69ce
timelyportfolio <kent.russell@timelyportfolio.com>
2019-01-27 13:48:32 -0600

    update leaflet.pm to 2.0.2

--- R/dependencies.R
+++ R/dependencies.R
@@ -6,7 +6,7 @@ pmDependencies <- function() {
   list(
     htmltools::htmlDependency(
       name = "leaflet-pm",
-      version = "1.2.1",
+      version = "2.0.2",
       src = "htmlwidgets/build",
       script = c("leaflet.pm.min.js", "lfx-pm-bindings.js"),
       stylesheet = "leaflet.pm.css",
